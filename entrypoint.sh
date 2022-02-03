6eee642b991e141c8b460714ad4da6489d11bf23
Ronnie Geraghty <ronniegerag@gmail.com>
2022-02-03 09:15:35 -0800

    testing on signed input

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@ set -e
 
 tag=${INPUT_TAG}
 message=${INPUT_MESSAGE}
-
+signed=${INPUT_SIGNED}
+echo signed
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
